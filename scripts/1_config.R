3ebe587710ef88bd93549eb8fa499cc5bbce88ec
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2024-12-24 11:17:23 +1100

    moves identify_ibra.R to config file and moves .R files to scripts/

--- scripts/1_config.R
+++ scripts/1_config.R
@@ -1,11 +1,33 @@
-# Finds IBRA regions that correspond to the Forests of East Australia hotspot 
+# libraries, options, config --------
+{
+  library(galah)
+  library(here)
+  library(dplyr)
+  library(readr)
+  library(purrr)
+  library(janitor)
+  library(sf)
+  library(ozmaps)
+}
 
+galah_config(email = Sys.getenv("ALA_EMAIL"))
+#sf_use_s2(FALSE)
+conflicted::conflicts_prefer(
+  dplyr::filter,
+  tidyr::unnest
+)
+
+source("functions.R")
+
+# identify IBRA --------
+# figure out which of the IBRA regions correspond to the Forests of East 
+# Australia hotspot 
 ea_hotspot <- st_read(here("data",
                            "spatial",
                            "hotspots_2016_1",
                            "hotspots_2016_1.shp")) |>
   filter(NAME == "Forests of East Australia")
-                         
+
 ibra <- st_read(here("data",
                      "spatial",
                      "ibra7",
